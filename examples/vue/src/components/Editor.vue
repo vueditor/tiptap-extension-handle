e1bbf1807a49d38ca18421bd9d012290abd456c8
liting <luz.liting@gmail.com>
2024-09-10 23:54:58 +0800

    chore(example): init vue example

---
+++ examples/vue/src/components/Editor.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+import { useEditor, EditorContent } from '@tiptap/vue-3'
+import StarterKit from '@tiptap/starter-kit'
+import { handle } from '../../../..'
+
+const editor = useEditor({
+  content: '<p>I’m running Tiptap with Vue.js. 🎉</p>',
+  extensions: [
+    StarterKit,
+    handle
+  ],
+})
+</script>
+
+<template>
+  <div class="editor">
+    <EditorContent :editor="editor" />
+  </div>
+</template>
+
+<style lang="scss">
+.editor {
+  box-shadow: 0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1);
+
+  .tiptap {
+    padding: 24px 64px;
+
+    &:focus-visible {
+      outline: none;
+    }
+  }
+}
+</style>
